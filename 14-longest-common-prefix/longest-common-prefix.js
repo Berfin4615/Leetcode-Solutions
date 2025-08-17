070572faf17e3085a1fcd390857ff2f8ce4b582f
Berfin <62778143+Berfin4615@users.noreply.github.com>
2025-08-17 17:12:01 +0300

    Time: 0 ms (100.00%) | Memory: 53 MB (93.69%) - LeetSync

--- 14-longest-common-prefix/longest-common-prefix.js
+++ 14-longest-common-prefix/longest-common-prefix.js
@@ -5,15 +5,14 @@
 var longestCommonPrefix = function(strs) {
     if (strs.length === 0) return "";
 
-    for (let i = 0; i < strs[0].length; i++) {
-        const char = strs[0][i];
+    let prefix = strs[0];
 
-        for (let j = 1; j < strs.length; j++) {
-            if (strs[j][i] !== char) {
-                return strs[0].slice(0, i);
-            }
+    for (let i = 1; i < strs.length; i++) {
+        while (strs[i].indexOf(prefix) !== 0) {
+            prefix = prefix.slice(0, -1); 
+            if (prefix === "") return "";
         }
     }
 
-    return strs[0];
+    return prefix;
 };
