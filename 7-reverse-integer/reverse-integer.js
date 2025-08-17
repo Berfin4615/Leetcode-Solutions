06b8a6be9be97dc81b93ad4d4569269b20118706
Berfin <62778143+Berfin4615@users.noreply.github.com>
2025-08-17 16:03:14 +0300

    Time: 36 ms (97.92%) | Memory: 55 MB (67.63%) - LeetSync

--- 7-reverse-integer/reverse-integer.js
+++ 7-reverse-integer/reverse-integer.js
@@ -5,9 +5,10 @@
 var reverse = function(x) {
     const limit = 2 ** 31;
     const reversed = parseInt(Math.abs(x).toString().split('').reverse().join(''));
-    const result = x < 0 ? -reversed : reversed;
-    if (result < -limit || result > limit - 1) {
+    if (reversed < -limit || reversed > limit - 1) {
         return 0;
+    } else {
+        const result = x < 0 ? -reversed : reversed;
+        return result;
     }
-    return result;
 };
