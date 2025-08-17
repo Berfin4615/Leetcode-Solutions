8549fba2e1fc0c1bc5fbda61b4843aab33ec0b10
Berfin <62778143+Berfin4615@users.noreply.github.com>
2025-08-17 15:57:34 +0300

    Time: 0 ms (100.00%) | Memory: 57.8 MB (41.37%) - LeetSync

--- 21-merge-two-sorted-lists/merge-two-sorted-lists.js
+++ 21-merge-two-sorted-lists/merge-two-sorted-lists.js
@@ -11,11 +11,6 @@
  * @return {ListNode}
  */
 
-function ListNode (val, next){
-    this.val = val === undefined ? 0 : val;
-    this.next = next === undefined ? null : next;
-}
-
 var mergeTwoLists = function(list1, list2) {
     let dummy = new ListNode(0);
     let current = dummy;
