c9d13fc1d33b7dbe5571d89c1b762ca4ad8f14d8
Berfin <62778143+Berfin4615@users.noreply.github.com>
2025-08-21 22:46:10 +0300

    Time: 0 ms (100.00%) | Memory: 52.9 MB (92.36%) - LeetSync

---
+++ 35-search-insert-position/search-insert-position.js
@@ -0,0 +1,17 @@
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number}
+ */
+var searchInsert = function(nums, target) {
+    if(nums.includes(target)){
+        return nums.indexOf(target);
+    } else {
+        for (let i = 0; i < nums.length; i++) {
+            if (target < nums[i]) {
+                return i;
+            }
+        }
+        return nums.length;
+    }
+};
