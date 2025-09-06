43d9b61036cecb09e884598f64875010b79ac154
Berfin <62778143+Berfin4615@users.noreply.github.com>
2025-09-06 23:31:37 +0300

    Time: 3 ms (50.97%) | Memory: 17.7 MB (59.69%) - LeetSync

--- 900-reordered-power-of-2/reordered-power-of-2.py
+++ 900-reordered-power-of-2/reordered-power-of-2.py
@@ -1,6 +1,8 @@
 class Solution:
     def reorderedPowerOf2(self, n: int) -> bool:
         sig = Counter(str(n))
+        if n == 2 or n == 1 or n == 4 or n == 8:
+            return True
         for k in range(31):
             if Counter(str(1 << k)) == sig:
                 return True
