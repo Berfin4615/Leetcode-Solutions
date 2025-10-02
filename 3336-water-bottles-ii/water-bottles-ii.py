ae15b05f43943e89ff63820710c478e8cc9bae3c
Berfin <62778143+Berfin4615@users.noreply.github.com>
2025-10-02 23:53:17 +0300

    Time: 32 ms (88.95%) | Memory: 17.9 MB (5.10%) - LeetSync

---
+++ 3336-water-bottles-ii/water-bottles-ii.py
@@ -0,0 +1,18 @@
+class Solution:
+    def maxBottlesDrunk(self, numBottles: int, numExchange: int) -> int:
+        drank = 0
+        empty = 0
+
+        while numBottles > 0:
+            drank += numBottles
+            empty += numBottles
+            numBottles = 0
+
+            if empty >= numExchange:
+                empty -= numExchange
+                numBottles = 1  
+                numExchange += 1 
+            else:
+                break
+
+        return drank
